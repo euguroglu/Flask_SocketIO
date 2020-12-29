662ed58b048bf7ceeb541705004579414bed4cd4
euguroglu <e.uguroglu@yahoo.com>
2020-12-29 19:39:31 +0300

    How to send message to js client from flask server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -15,6 +15,8 @@ def index():
 @socketio.on('message')
 def receive_message(message):
     print('#######: {}'.format(message))
+#Command to send message from Flask to javascript client
+    send('This is a message from Flask')
 
 if __name__ == "__main__":
     socketio.run(app)
--- static/script.js
+++ static/script.js
@@ -1,5 +1,10 @@
 var socket = io.connect('http://127.0.0.1:5000')
 
+// Code to send message to flask server
 socket.on('connect',function() {
   socket.send('I am now connected!');
+// Code to receive message from flask server
+  socket.on('message', function(msg) {
+    alert(msg);
+  });
 });
